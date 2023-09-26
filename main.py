922cae5f2c7701776f6c641d11ee4689838f5ad6
nvoff <nikita.voxmincev@mail.ru>
2023-09-26 15:50:07 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,156 @@
+import json
+from aiogram import Bot, Dispatcher, executor, types
+
+
+def parse_env(file_path: str):
+    from pathlib import Path
+    file = Path(file_path)
+    if file.is_file():
+        data = file.open('r').readlines()
+        data = [line.strip() for line in data]
+        data = [line for line in data if line and not line.startswith('#')]
+        data = [line.split('=') for line in data]
+        data = {line[0]: line[1].replace('"', '') for line in data}
+        return data
+    else:
+        return {}
+        
+        
+data = parse_env('test.env')
+
+bot = Bot(
+    token=data['BOT_TOKEN'],
+    timeout=1000
+)
+dp = Dispatcher(bot, no_throttle_error=True)
+
+statuses = {}
+
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    await bot.send_message(
+        chat_id=message.chat.id,
+        text='Добро пожаловать в *ORG Бота*!\n\nС его помощью вы можете отправить заявку сотрудникам организационно-технического отдела',
+#        parse_mode=types.ParseMode.MARKDOWN
+    )
+
+
+@dp.message_handler(content_types=types.ContentType.ANY)
+async def message(message: types.Message):
+    
+    admin_group = -1001869659573
+    
+    if message.chat.id != admin_group:
+        
+        await bot.send_message(
+            chat_id=message.chat.id,
+            text='Ваш запрос был отправлен специалистам, ожидайте ответа!',
+#            parse_mode=types.ParseMode.MARKDOWN
+        )
+        
+        if str(message.chat.id) not in statuses or statuses.get(str(message.chat.id)) != 'inprogress':
+            await bot.send_message(
+                chat_id=admin_group,
+                text='Обращение: \n\n'+message.text+'\n\nОтправитель: ' + '\n— ID: ' + str(message.from_user.id) + '\n— Telegram: @' + message.from_user.username + '\n— Name: ' + str(message.from_user.first_name) + ' ' + str(message.from_user.last_name) +"\n\nВыберите действие",
+#                parse_mode=types.ParseMode.MARKDOWN,
+                reply_markup=types.InlineKeyboardMarkup().add(
+                    types.InlineKeyboardButton(text='В работе', callback_data='adm_act_inprogress_'+str(message.chat.id))
+                ).add(
+                    types.InlineKeyboardButton(text='Решено', callback_data='adm_act_solve_'+str(message.chat.id)),
+                    types.InlineKeyboardButton(text='Отклонить', callback_data='adm_act_reject_'+str(message.chat.id))
+                )
+            )
+            
+        else:
+            
+            await bot.forward_message(
+                chat_id=admin_group,
+                from_chat_id=message.chat.id,
+                message_id=message.message_id
+            )
+        
+    else:
+        
+        reply = message.reply_to_message
+        
+        if reply is not None:
+            if reply.from_user.is_bot:
+                
+                if reply.reply_markup is not None:
+                    markup = reply.reply_markup['inline_keyboard']
+                    uid = markup[0][0]['callback_data'].split('_')[3]
+                    await bot.send_message(
+                        chat_id=int(uid),
+                        text="Вопрос от специалиста:\n\n_" + message.text + "_",
+ #                       parse_mode=types.ParseMode.MARKDOWN
+                    )
+    
+    
+@dp.callback_query_handler(lambda c: c.data.startswith('adm_act_'))
+async def inprogress(callback_query: types.CallbackQuery):
+    await bot.answer_callback_query(callback_query.id)
+    
+    command = callback_query.data.replace('adm_act_', '').split('_')
+    
+    uid = int(command[1])
+    
+    if command[0] == 'inprogress':
+        statuses[str(uid)] = 'inprogress'
+        await bot.send_message(
+            chat_id=uid,
+            text='Ваш запрос в работе!',
+#            parse_mode=types.ParseMode.MARKDOWN
+        )
+        await bot.edit_message_text(
+            chat_id=callback_query.message.chat.id,
+            message_id=callback_query.message.message_id,
+#            parse_mode=types.ParseMode.MARKDOWN,
+            text=callback_query.message.text.replace('\n\nВыберите действие', '') + '\n\nИстория действий\n\nℹ️ Запрос в работе!',
+            reply_markup=types.InlineKeyboardMarkup().add(
+                types.InlineKeyboardButton(text='Решено', callback_data='adm_act_solve_'+str(uid)),
+                types.InlineKeyboardButton(text='Отклонить', callback_data='adm_act_reject_'+str(uid))
+            )
+        )
+    elif command[0] == 'solve':
+        try: 
+            del statuses[str(uid)]
+        except:
+            pass
+        await bot.send_message(
+            chat_id=uid,
+            text='Ваш запрос решен!',
+#            parse_mode=types.ParseMode.MARKDOWN
+        )
+        text_ = callback_query.message.text
+        if "Выберите действие" in text_:
+            text_ = text_.replace('Выберите действие', 'История действий')
+        await bot.edit_message_text(
+            chat_id=callback_query.message.chat.id,
+            message_id=callback_query.message.message_id,
+            text=text_ + '\n✅ Запрос решен!',
+            reply_markup=None,
+#            parse_mode=types.ParseMode.MARKDOWN
+        )
+    elif command[0] == 'reject':
+        try: 
+            del statuses[str(uid)]
+        except:
+            pass
+        await bot.send_message(
+            chat_id=uid,
+            text='Ваш запрос отклонен!',
+#            parse_mode=types.ParseMode.MARKDOWN
+        )
+        text_ = callback_query.message.text
+        if "Выберите действие" in text_:
+            text_ = text_.replace('Выберите действие', 'История действий\n\n')
+        await bot.edit_message_text(
+            chat_id=callback_query.message.chat.id,
+            message_id=callback_query.message.message_id,
+            text=text_ + '\n❌ Отклонён!',
+            reply_markup=None,
+#            parse_mode=types.ParseMode.MARKDOWN
+        )
+
+executor.start_polling(dp, skip_updates=True, timeout=1000)
